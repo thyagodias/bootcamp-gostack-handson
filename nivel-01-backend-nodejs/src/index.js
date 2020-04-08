7d1bd16d55dd29bf4e8a696771ec9977f75cedf5
Thyago Dias <thyago.dias@unemat.br>
2020-04-08 01:24:32 -0400

    cria o servidor com os recursos de projetos

---
+++ nivel-01-backend-nodejs/src/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const { uuid } = require('uuidv4')
+
+const app = express()
+app.use(express.json())
+
+const projects = []
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Server only' })
+})
+
+// List
+app.get('/projects', (request, response) => {
+  return response.json({ projects })
+})
+
+// Create
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body
+  const project = { id: uuid(), title, owner }
+  projects.push(project)
+
+  return response.status(201).json({ project })
+})
+
+// Update
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const { title, owner } = request.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado' })
+  }
+
+  const project = { id, title, owner }
+
+  projects[projectIndex] = project
+
+  return response.json({ project })
+})
+
+// Delete
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Back-end started!')
+})
