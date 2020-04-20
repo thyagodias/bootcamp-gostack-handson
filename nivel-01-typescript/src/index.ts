5d9a6cd8f8eac62f266d644bcec9e38f137f4e3f
Thyago Dias <thyago.dias@unemat.br>
2020-04-20 01:40:50 -0400

    cria arquivo de rota para testar tipos do express

--- nivel-01-typescript/src/index.ts
+++ nivel-01-typescript/src/index.ts
@@ -1,9 +1,8 @@
 import express from 'express'
+import { helloWorld } from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json('ok')
-})
+app.get('/', helloWorld)
 
 app.listen()
